33d5ab9669a91631f5d53def92318521f1400394
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-07-25 20:31:42 +0900

    Define queries and relevant manipulations

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 )
 
+var tables map[string]*table
+
 type column struct {
 	parent string
 	name   string
@@ -54,7 +56,9 @@ func (r *relation) findColumn(name string) int {
 			return i
 		}
 	}
-	return -1
+	// we can simplify checking the existence of n in r,
+	// by r.findColumn(n) <= len(r.columns) before random accesses
+	return len(r.columns)
 }
 
 type table struct {
@@ -75,13 +79,102 @@ func create(name string, colNames []string) *table {
 	for _, cn := range colNames {
 		cols = append(cols, newColumn(cn))
 	}
-	// FIXME: register table to a global dictionary
-	return newTable(name, cols)
+	t := newTable(name, cols)
+	tables[name] = t
+	return t
 }
 
 func (t *table) insert(vals []interface{}) *table {
 	t.tuples = append(t.tuples, newTuple(vals))
 	return t
+}
+
+type query struct {
+	relation
+}
+
+func newQuery(cols []*column, tups []*tuple) *query {
+	var q *query
+	q.columns = cols
+	q.tuples = tups
+	return q
+}
+
+func from(tableName string) *query {
+	t := tables[tableName]
+	newCols := make([]*column, len(t.columns))
+	copy(newCols, t.columns)
+	return newQuery(newCols, t.tuples)
+}
+
+func (q *query) selectQ(colNames ...string) *query {
+	newCols := []*column{}
+	idxs := []int{}
+	for _, cn := range colNames {
+		newCols = append(newCols, newColumn(cn))
+		idxs = append(idxs, q.findColumn(cn))
+	}
+	newTups := []*tuple{}
+	for _, tup := range q.tuples {
+		vals := []interface{}{}
+		for _, idx := range idxs {
+			// TODO: Can I avoid to refer the nil pointer?
+			if idx < len(tup.values) {
+				vals = append(vals, tup.values[idx])
+			} else {
+				vals = append(vals, nil)
+			}
+		}
+		newTups = append(newTups, newTuple(vals))
+	}
+	return newQuery(newCols, newTups)
+}
+
+func (q *query) leftJoin(tableName, colName string) *query {
+	t := tables[tableName]
+	newCols := []*column{}
+	newCols = append(newCols, q.columns...)
+	newCols = append(newCols, t.columns...)
+	lIdx, rIdx := q.findColumn(colName), t.findColumn(colName)
+	if len(q.columns) <= lIdx || len(t.columns) <= rIdx {
+		return newQuery(newCols, []*tuple{})
+	}
+	newTups := []*tuple{}
+	for _, lTup := range q.tuples {
+		if len(lTup.values) <= lIdx {
+			continue
+		}
+		keyVal := lTup.values[lIdx]
+		// the remaining values are filled by nil
+		vals := make([]interface{}, len(newCols))
+		copy(vals, lTup.values)
+		for _, rTup := range t.tuples {
+			if len(rTup.values) <= rIdx {
+				continue
+			}
+			if rTup.values[rIdx] == keyVal {
+				vals = append(vals, rTup.values)
+				break // join at most one tuple from the rightside table
+			}
+		}
+		newTups = append(newTups, newTuple(vals))
+	}
+	return newQuery(newCols, newTups)
+}
+
+func (q *query) lessThan(colName string, n int) *query {
+	idx := q.findColumn(colName)
+	if idx >= len(q.columns) {
+		return newQuery(q.columns, []*tuple{})
+	}
+	newTups := []*tuple{}
+	for _, tup := range q.tuples {
+		v, ok := tup.values[idx].(int)
+		if ok && v < n {
+			newTups = append(newTups, tup)
+		}
+	}
+	return newQuery(q.columns, newTups)
 }
 
 func main() {
