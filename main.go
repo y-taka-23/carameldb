1ca3255248548822af1b250be70a8fd841ce1bf4
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-07-27 04:53:35 +0900

    [WIP] Implement subqueries

--- main.go
+++ main.go
@@ -14,8 +14,9 @@ func main() {
 	items.insert(1, "apple", 1, 300)
 	items.insert(2, "orange", 1, 130)
 	items.insert(3, "cabbage", 2, 200)
-	items.insert(4, "seaweed", nil, 250)
-	items.insert(5, "mushroom", 3, 180)
+	items.insert(4, "saury", 3, 220)
+	items.insert(5, "seaweed", nil, 250)
+	items.insert(6, "mushroom", 3, 180)
 
 	types := create(
 		"types",
@@ -23,12 +24,20 @@ func main() {
 	)
 	types.insert(1, "fruit")
 	types.insert(2, "vegetable")
+	types.insert(3, "fish")
 
 	fmt.Println(items)
 	fmt.Println(from("items"))
 	fmt.Println(from("items").selectQ("item_name", "price"))
 	fmt.Println(from("items").lessThan("price", 250))
 	fmt.Println(from("items").leftJoin("types", "type_id"))
+	fmt.Println(
+		from(
+			from("items").lessThan(250),
+		).leftJoin(
+			from("types").lessThan(3), "type_id",
+		),
+	)
 }
 
 var tables = map[string]*table{}
@@ -50,9 +59,128 @@ func newTuple(vals []interface{}) *tuple {
 	return &tuple{values: vals}
 }
 
+type queryObj interface {
+	leftJoin(qo queryObjer) queryObj
+	lessThan(colName string, n int) queryObj
+	selectQ(colNames ...string) queryObj
+}
+
+type queryObjer interface {
+	queryObj() queryObj
+}
+
+type tableName string
+
+func (s tableName) queryObj() {
+	return tables[s]
+}
+
+func (qo *queryObj) queryObj() {
+	return qo
+}
+
+func from(q *queryObjer) *queryObj {
+	return q.queryObj()
+}
+
 type relation struct {
 	columns []*column
 	tuples  []*tuple
+}
+
+func newRelation(cols []*column, tups []*tuple) *relation {
+	return &relation{columns: cols, tuples: tups}
+}
+
+func (r *relation) findColumn(name string) int {
+	for i, c := range r.columns {
+		if c.name == name {
+			return i
+		}
+	}
+	// we can simplify checking the existence of n in r,
+	// by r.findColumn(n) <= len(r.columns) before random accesses
+	return len(r.columns)
+}
+
+func (r *relation) selectQ(colNames ...string) *queryObj {
+	idxs := []int{}
+	newCols := []*column{}
+	for _, cn := range colNames {
+		idx := r.findColumn(cn)
+		idxs = append(idxs, idx)
+		if idx < len(r.columns) {
+			newCols = append(newCols, r.columns[idx])
+		}
+	}
+	newTups := []*tuple{}
+	for _, tup := range r.tuples {
+		vals := []interface{}{}
+		for _, idx := range idxs {
+			// TODO: Can I avoid to refer the nil pointer?
+			if idx < len(tup.values) {
+				vals = append(vals, tup.values[idx])
+			} else {
+				vals = append(vals, nil)
+			}
+		}
+		newTups = append(newTups, newTuple(vals))
+	}
+	return newRelation(newCols, newTups)
+}
+
+func (r *relation) leftJoin(tblName, colName string) *queryObj {
+	t := tables[tblName]
+	newCols := []*column{}
+	newCols = append(newCols, r.columns...)
+	for _, c := range t.columns {
+		newCols = append(newCols, newColumn(tblName, c.name))
+	}
+	rIdx, tIdx := r.findColumn(colName), t.findColumn(colName)
+	if len(r.columns) <= rIdx || len(t.columns) <= tIdx {
+		return newRelation(newCols, []*tuple{})
+	}
+	newTups := []*tuple{}
+	for _, rTup := range r.tuples {
+		if len(rTup.values) <= rIdx {
+			continue
+		}
+		keyVal := rTup.values[rIdx]
+		vals := []interface{}{}
+		vals = append(vals, rTup.values...)
+		// join at non-nil values only
+		if keyVal != nil {
+			for _, tTup := range t.tuples {
+				if len(tTup.values) <= tIdx {
+					continue
+				}
+				if tTup.values[tIdx] == keyVal {
+					vals = append(vals, tTup.values...)
+					break // join at most one tuple from the rightside table
+				}
+			}
+		}
+		for len(vals) < len(newCols) {
+			vals = append(vals, nil)
+		}
+		newTups = append(newTups, newTuple(vals))
+	}
+	return newRelation(newCols, newTups)
+}
+
+func (r *relation) lessThan(colName string, n int) *queryObj {
+	idx := r.findColumn(colName)
+	if idx >= len(r.columns) {
+		return newRelation(r.columns, []*tuple{})
+	}
+	newTups := []*tuple{}
+	for _, tup := range r.tuples {
+		v, ok := tup.values[idx].(int)
+		if ok && v < n {
+			newTups = append(newTups, tup)
+		}
+	}
+	return newRelation(q.columns, newTups)
 }
 
 func (r *relation) String() string {
@@ -76,23 +204,12 @@ func (r *relation) String() string {
 	return buf.String()
 }
 
-func (r *relation) findColumn(name string) int {
-	for i, c := range r.columns {
-		if c.name == name {
-			return i
-		}
-	}
-	// we can simplify checking the existence of n in r,
-	// by r.findColumn(n) <= len(r.columns) before random accesses
-	return len(r.columns)
-}
-
 type table struct {
 	relation
 	name string
 }
 
-func newTable(name string, cols []*column) *table {
+func newTable(name tableName, cols []*column) *table {
 	t := &table{}
 	t.name = name
 	t.columns = cols
@@ -100,7 +217,7 @@ func newTable(name string, cols []*column) *table {
 	return t
 }
 
-func create(name string, colNames []string) *table {
+func create(name tableName, colNames []string) *table {
 	cols := []*column{}
 	for _, cn := range colNames {
 		cols = append(cols, newColumn("", cn))
@@ -113,104 +230,4 @@ func create(name string, colNames []string) *table {
 func (t *table) insert(vals ...interface{}) *table {
 	t.tuples = append(t.tuples, newTuple(vals))
 	return t
-}
-
-type query struct {
-	relation
-}
-
-func newQuery(cols []*column, tups []*tuple) *query {
-	q := &query{}
-	q.columns = cols
-	q.tuples = tups
-	return q
-}
-
-func from(tableName string) *query {
-	t := tables[tableName]
-	newCols := []*column{}
-	for _, c := range t.columns {
-		newCols = append(newCols, newColumn(t.name, c.name))
-	}
-	return newQuery(newCols, t.tuples)
-}
-
-func (q *query) selectQ(colNames ...string) *query {
-	idxs := []int{}
-	newCols := []*column{}
-	for _, cn := range colNames {
-		idx := q.findColumn(cn)
-		idxs = append(idxs, idx)
-		if idx < len(q.columns) {
-			newCols = append(newCols, q.columns[idx])
-		}
-	}
-	newTups := []*tuple{}
-	for _, tup := range q.tuples {
-		vals := []interface{}{}
-		for _, idx := range idxs {
-			// TODO: Can I avoid to refer the nil pointer?
-			if idx < len(tup.values) {
-				vals = append(vals, tup.values[idx])
-			} else {
-				vals = append(vals, nil)
-			}
-		}
-		newTups = append(newTups, newTuple(vals))
-	}
-	return newQuery(newCols, newTups)
-}
-
-func (q *query) leftJoin(tableName, colName string) *query {
-	t := tables[tableName]
-	newCols := []*column{}
-	newCols = append(newCols, q.columns...)
-	for _, c := range t.columns {
-		newCols = append(newCols, newColumn(tableName, c.name))
-	}
-	lIdx, rIdx := q.findColumn(colName), t.findColumn(colName)
-	if len(q.columns) <= lIdx || len(t.columns) <= rIdx {
-		return newQuery(newCols, []*tuple{})
-	}
-	newTups := []*tuple{}
-	for _, lTup := range q.tuples {
-		if len(lTup.values) <= lIdx {
-			continue
-		}
-		keyVal := lTup.values[lIdx]
-		vals := []interface{}{}
-		vals = append(vals, lTup.values...)
-		// join at non-nil values only
-		if keyVal != nil {
-			for _, rTup := range t.tuples {
-				if len(rTup.values) <= rIdx {
-					continue
-				}
-				if rTup.values[rIdx] == keyVal {
-					vals = append(vals, rTup.values...)
-					break // join at most one tuple from the rightside table
-				}
-			}
-		}
-		for len(vals) < len(newCols) {
-			vals = append(vals, nil)
-		}
-		newTups = append(newTups, newTuple(vals))
-	}
-	return newQuery(newCols, newTups)
-}
-
-func (q *query) lessThan(colName string, n int) *query {
-	idx := q.findColumn(colName)
-	if idx >= len(q.columns) {
-		return newQuery(q.columns, []*tuple{})
-	}
-	newTups := []*tuple{}
-	for _, tup := range q.tuples {
-		v, ok := tup.values[idx].(int)
-		if ok && v < n {
-			newTups = append(newTups, tup)
-		}
-	}
-	return newQuery(q.columns, newTups)
 }
